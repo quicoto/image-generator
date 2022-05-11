256e4f0118e66ddf24872e518b5e96c4e9bfb95e
Ricard Torres <torres.rick@gmail.com>
2022-05-11 12:55:44 +0200

    Documentation

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# Node open graph image generator
+# Node image generator
 
-Generates images with a given text and categories
+Generates images with a given text and an ID using canvas.
+
+## [Open Graph](https://ogp.me/) and WordPress
+
+⚠️ You don't have to integrate with WordPress if you don't want to. Fork the project and use it as a standalone or with another data source.
+
+The database (source of the script) is generated based on WordPress posts titles with their ID's using the [generate-posts-ids.php](blob/main/generate-posts-ids.php)
+
+Then by adding the [overwrite-open-graph.php](blob/main/overwrite-open-graph) you can overwrite the [Yoast SEO](https://wordpress.org/plugins/wordpress-seo/) plugin output for the Open Graph image.
+
+Should the image file exist on disk, it will look something like:
+
+```html
+<meta property="og:image" content="https://ricard.dev/wp-content/themes/ricks-code/image-generator/images/9105.png" />
+```
+
+You can either place this script inside your WordPress theme or use a WordPress plugin.
+
+## How it works?
+
+1. Get an array of images to create (title+id) from [database.txt](blob/main/database.txt)
+2. Create a canvas with background color
+3. Defines font sizes and colors
+4. Adds a text to the canvas, check for word wrapping.
+5. Creates the image (PNG) file to disk
+
+### Throttle
+
+The code has a setTimeout to throttle the process and prevent from being killed in certain environments. You may customize the delay.
--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const { createCanvas, loadImage } = require('canvas');
 const rimraf = require('rimraf');
 const utils = require('./utils');
 
+// Configuration
 const delay = 1000; // Throttle to prevent memory issues
 let throttle = 0;
-const width = 1200;
-const height = 630;
-const titleMaxWidth = 1000;
-const titleLineHeight = 100;
+const canvas = {
+  height: 630,
+  width: 1200,
+};
+const title = {
+  maxWidth: 1000,
+  lineHeight: 100,
+};
 const paths = {
   images: './images',
   database: './database.txt',
@@ -23,47 +28,63 @@ const colors = {
   postTitle: '#fd7e14',
   site: '#fff',
 };
+const siteName = 'ricard.dev';
 
 // Delete the directory content
 rimraf.sync(paths.images);
 
-// Create the requried folders
+// Create the required folder
 fs.mkdir(paths.images, () => {});
 
+// Read the database file and get all the lines in an array
 const databaseContent = fs.readFileSync(paths.database, { encoding: 'utf8', flag: 'r' });
 const lines = utils.getLines(databaseContent);
 
 lines.forEach((line) => {
   setTimeout(() => {
-    const imageCanvas = createCanvas(width, height);
+    const imageCanvas = createCanvas(canvas.width, canvas.height);
     const context = imageCanvas.getContext('2d');
-    const content = line.split('|');
-    const postId = content[0];
+    const content = line.split('|'); // Example line: "401|Autofocus in HTML5"
+    const postId = content[0]; // WordPress Post ID
     const postTitle = content[1];
     let textWidth;
 
     // eslint-disable-next-line no-console
     console.log(`Starting time: ${new Date().toLocaleString()} - ${postTitle}`);
 
+    // Define the canvas background
     context.fillStyle = colors.background;
-    context.fillRect(0, 0, width, height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
+    // Define the post title text
     context.font = fonts.postTitle;
     context.textAlign = 'center';
     context.textBaseline = 'top';
 
+    // Define the area of the text
     context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120);
-    context.fillStyle = colors.postTitle;
-    utils.wrapText(context, postTitle, 600, 80, titleMaxWidth, titleLineHeight);
 
+    // Color the text
+    context.fillStyle = colors.postTitle;
+
+    // Fills the text with appriate word wrapping
+    utils.wrapText(context, postTitle, 600, 80, title.maxWidth, title.lineHeight);
+
+    // Define the site name text
     context.fillStyle = colors.site;
     context.font = fonts.site;
-    context.fillText('ricard.dev', 580, 520);
+    context.fillText(siteName, 580, 520);
 
+    // Load the profile image next to the site name
     loadImage(paths.profileImage).then((image) => {
+      // Defines the image position and size
       context.drawImage(image, 350, 510, 70, 70);
-      const buffer = imageCanvas.toBuffer('image/png');
-      fs.writeFileSync(`${paths.images}/${postId}.png`, buffer);
+
+      // Create the final image
+      fs.writeFileSync(
+        `${paths.images}/${postId}.png`,
+        imageCanvas.toBuffer('image/png'),
+      );
     });
   }, throttle);
 
